78aa9673091a9d91599fe214ef84c0532adee5da
mrp44rth <113108394+mrp44rth@users.noreply.github.com>
2024-03-13 22:48:42 +0530

    Add files via upload

---
+++ Fourth-Batch/js.js
@@ -0,0 +1,113 @@
+// Sample data (you can replace this with your actual data)
+let i = 1;
+const data = [
+ // add data here
+[i++, "aditya Mali", "69242702", "./Fourth Batch/adityaMali 69242702/1.bmp", "./Fourth Batch/adityaMali 69242702/2.bmp", "./Fourth Batch/adityaMali 69242702/3.bmp", "./Fourth Batch/adityaMali 69242702/4.bmp", "./Fourth Batch/adityaMali 69242702/5.bmp"],
+[i++, "Aneeta", "51013729", "./Fourth Batch/Aneeta 51013729/1.bmp", "./Fourth Batch/Aneeta 51013729/2.bmp", "./Fourth Batch/Aneeta 51013729/3.bmp", "./Fourth Batch/Aneeta 51013729/4.bmp", "./Fourth Batch/Aneeta 51013729/5.bmp"],
+[i++, "Ankit Bharti", "97118774", "./Fourth Batch/AnkitBharti 97118774/1.bmp", "./Fourth Batch/AnkitBharti 97118774/2.bmp", "./Fourth Batch/AnkitBharti 97118774/3.bmp", "./Fourth Batch/AnkitBharti 97118774/4.bmp", "./Fourth Batch/AnkitBharti 97118774/5.bmp"],
+[i++, "Anubhav Bharti", "98727946", "./Fourth Batch/AnubhavBharti 98727946/1.bmp", "./Fourth Batch/AnubhavBharti 98727946/2.bmp", "./Fourth Batch/AnubhavBharti 98727946/3.bmp", "./Fourth Batch/AnubhavBharti 98727946/4.bmp", "./Fourth Batch/AnubhavBharti 98727946/5.bmp"],
+[i++, "Chandra Bhan Yadav", "02160507", "./Fourth Batch/ChandraBhanYadav 02160507/1.bmp", "./Fourth Batch/ChandraBhanYadav 02160507/2.bmp", "./Fourth Batch/ChandraBhanYadav 02160507/3.bmp", "./Fourth Batch/ChandraBhanYadav 02160507/4.bmp", "./Fourth Batch/ChandraBhanYadav 02160507/5.bmp"],
+[i++, "Gaurav Kashyap", "94909024", "./Fourth Batch/GauravKashyap 94909024/1.bmp", "./Fourth Batch/GauravKashyap 94909024/2.bmp", "./Fourth Batch/GauravKashyap 94909024/3.bmp", "./Fourth Batch/GauravKashyap 94909024/4.bmp", "./Fourth Batch/GauravKashyap 94909024/5.bmp"],
+[i++, "gautam sahu", "77236416", "./Fourth Batch/gautamsahu 77236416/1.bmp", "./Fourth Batch/gautamsahu 77236416/2.bmp", "./Fourth Batch/gautamsahu 77236416/3.bmp", "./Fourth Batch/gautamsahu 77236416/4.bmp", "./Fourth Batch/gautamsahu 77236416/5.bmp"],
+[i++, "Mukul Yadav", "23704402", "./Fourth Batch/MukulYadav 23704402/1.bmp", "./Fourth Batch/MukulYadav 23704402/2.bmp", "./Fourth Batch/MukulYadav 23704402/3.bmp", "./Fourth Batch/MukulYadav 23704402/4.bmp", "./Fourth Batch/MukulYadav 23704402/5.bmp"],
+[i++, "Muskan Maurya", "48436011", "./Fourth Batch/MuskanMaurya 48436011/1.bmp", "./Fourth Batch/MuskanMaurya 48436011/2.bmp", "./Fourth Batch/MuskanMaurya 48436011/3.bmp", "./Fourth Batch/MuskanMaurya 48436011/4.bmp", "./Fourth Batch/MuskanMaurya 48436011/5.bmp"],
+[i++, "Nihal Chaurasiya", "71768419", "./Fourth Batch/NihalChaurasiya 71768419/1.bmp", "./Fourth Batch/NihalChaurasiya 71768419/2.bmp", "./Fourth Batch/NihalChaurasiya 71768419/3.bmp", "./Fourth Batch/NihalChaurasiya 71768419/4.bmp", "./Fourth Batch/NihalChaurasiya 71768419/5.bmp"],
+[i++, "Nikhil Kumar", "00735178", "./Fourth Batch/NikhilKumar 00735178/1.bmp", "./Fourth Batch/NikhilKumar 00735178/2.bmp", "./Fourth Batch/NikhilKumar 00735178/3.bmp", "./Fourth Batch/NikhilKumar 00735178/4.bmp", "./Fourth Batch/NikhilKumar 00735178/5.bmp"],
+[i++, "Pooja", "56974964", "./Fourth Batch/Pooja 56974964/1.bmp", "./Fourth Batch/Pooja 56974964/2.bmp", "./Fourth Batch/Pooja 56974964/3.bmp", "./Fourth Batch/Pooja 56974964/4.bmp", "./Fourth Batch/Pooja 56974964/5.bmp"],
+[i++, "Pravin Mishra", "60163422", "./Fourth Batch/PravinMishra 60163422/1.bmp", "./Fourth Batch/PravinMishra 60163422/2.bmp", "./Fourth Batch/PravinMishra 60163422/3.bmp", "./Fourth Batch/PravinMishra 60163422/4.bmp", "./Fourth Batch/PravinMishra 60163422/5.bmp"],
+[i++, "Priya", "38886869", "./Fourth Batch/Priya 38886869/1.bmp", "./Fourth Batch/Priya 38886869/2.bmp", "./Fourth Batch/Priya 38886869/3.bmp", "./Fourth Batch/Priya 38886869/4.bmp", "./Fourth Batch/Priya 38886869/5.bmp"],
+[i++, "Priyanka", "70778417", "./Fourth Batch/Priyanka 70778417/1.bmp", "./Fourth Batch/Priyanka 70778417/2.bmp", "./Fourth Batch/Priyanka 70778417/3.bmp", "./Fourth Batch/Priyanka 70778417/4.bmp", "./Fourth Batch/Priyanka 70778417/5.bmp"],
+[i++, "Raj Gond", "48275018", "./Fourth Batch/RajGond 48275018/1.bmp", "./Fourth Batch/RajGond 48275018/2.bmp", "./Fourth Batch/RajGond 48275018/3.bmp", "./Fourth Batch/RajGond 48275018/4.bmp", "./Fourth Batch/RajGond 48275018/5.bmp"],
+[i++, "Sachin Sonkar", "29094801", "./Fourth Batch/SachinSonkar 29094801/1.bmp", "./Fourth Batch/SachinSonkar 29094801/2.bmp", "./Fourth Batch/SachinSonkar 29094801/3.bmp", "./Fourth Batch/SachinSonkar 29094801/4.bmp", "./Fourth Batch/SachinSonkar 29094801/5.bmp"],
+[i++, "Satish Maurya", "69045881", "./Fourth Batch/SatishMaurya 69045881/1.bmp", "./Fourth Batch/SatishMaurya 69045881/2.bmp", "./Fourth Batch/SatishMaurya 69045881/3.bmp", "./Fourth Batch/SatishMaurya 69045881/4.bmp", "./Fourth Batch/SatishMaurya 69045881/5.bmp"],
+[i++, "Satyam Singh", "14360950", "./Fourth Batch/SatyamSingh 14360950/1.bmp", "./Fourth Batch/SatyamSingh 14360950/2.bmp", "./Fourth Batch/SatyamSingh 14360950/3.bmp", "./Fourth Batch/SatyamSingh 14360950/4.bmp", "./Fourth Batch/SatyamSingh 14360950/5.bmp"],
+[i++, "Saurabh Yadav", "59251741", "./Fourth Batch/SaurabhYadav 59251741/1.bmp", "./Fourth Batch/SaurabhYadav 59251741/2.bmp", "./Fourth Batch/SaurabhYadav 59251741/3.bmp", "./Fourth Batch/SaurabhYadav 59251741/4.bmp", "./Fourth Batch/SaurabhYadav 59251741/5.bmp"],
+[i++, "Savita Nigam", "63096380", "./Fourth Batch/SavitaNigam 63096380/1.bmp", "./Fourth Batch/SavitaNigam 63096380/2.bmp", "./Fourth Batch/SavitaNigam 63096380/3.bmp", "./Fourth Batch/SavitaNigam 63096380/4.bmp", "./Fourth Batch/SavitaNigam 63096380/5.bmp"],
+[i++, "Seema Sahu", "08979873", "./Fourth Batch/SeemaSahu 08979873/1.bmp", "./Fourth Batch/SeemaSahu 08979873/2.bmp", "./Fourth Batch/SeemaSahu 08979873/3.bmp", "./Fourth Batch/SeemaSahu 08979873/4.bmp", "./Fourth Batch/SeemaSahu 08979873/5.bmp"],
+[i++, "Shubham kumar Rao", "06987394", "./Fourth Batch/ShubhamkumarRao 06987394/1.bmp", "./Fourth Batch/ShubhamkumarRao 06987394/2.bmp", "./Fourth Batch/ShubhamkumarRao 06987394/3.bmp", "./Fourth Batch/ShubhamkumarRao 06987394/4.bmp", "./Fourth Batch/ShubhamkumarRao 06987394/5.bmp"],
+[i++, "Sonam", "83022324", "./Fourth Batch/Sonam 83022324/1.bmp", "./Fourth Batch/Sonam 83022324/2.bmp", "./Fourth Batch/Sonam 83022324/3.bmp", "./Fourth Batch/Sonam 83022324/4.bmp", "./Fourth Batch/Sonam 83022324/5.bmp"],
+[i++, "Soni", "45937787", "./Fourth Batch/Soni 45937787/1.bmp", "./Fourth Batch/Soni 45937787/2.bmp", "./Fourth Batch/Soni 45937787/3.bmp", "./Fourth Batch/Soni 45937787/4.bmp", "./Fourth Batch/Soni 45937787/5.bmp"],
+[i++, "Sujeet Yadav", "54817551", "./Fourth Batch/SujeetYadav 54817551/1.bmp", "./Fourth Batch/SujeetYadav 54817551/2.bmp", "./Fourth Batch/SujeetYadav 54817551/3.bmp", "./Fourth Batch/SujeetYadav 54817551/4.bmp", "./Fourth Batch/SujeetYadav 54817551/5.bmp"],
+[i++, "Suraj Kumar Yadav", "48077868", "./Fourth Batch/SurajKumarYadav 48077868/1.bmp", "./Fourth Batch/SurajKumarYadav 48077868/2.bmp", "./Fourth Batch/SurajKumarYadav 48077868/3.bmp", "./Fourth Batch/SurajKumarYadav 48077868/4.bmp", "./Fourth Batch/SurajKumarYadav 48077868/5.bmp"],
+[i++, "Umang parashar", "67866314", "./Fourth Batch/Umangparashar 67866314/1.bmp", "./Fourth Batch/Umangparashar 67866314/2.bmp", "./Fourth Batch/Umangparashar 67866314/3.bmp", "./Fourth Batch/Umangparashar 67866314/4.bmp", "./Fourth Batch/Umangparashar 67866314/5.bmp"],
+[i++, "Vaibhav Gupta", "91182976", "./Fourth Batch/VaibhavGupta 91182976/1.bmp", "./Fourth Batch/VaibhavGupta 91182976/2.bmp", "./Fourth Batch/VaibhavGupta 91182976/3.bmp", "./Fourth Batch/VaibhavGupta 91182976/4.bmp", "./Fourth Batch/VaibhavGupta 91182976/5.bmp"],
+[i++, "Vicky Nishad", "09380241", "./Fourth Batch/VickyNishad 09380241/1.bmp", "./Fourth Batch/VickyNishad 09380241/2.bmp", "./Fourth Batch/VickyNishad 09380241/3.bmp", "./Fourth Batch/VickyNishad 09380241/4.bmp", "./Fourth Batch/VickyNishad 09380241/5.bmp"],
+
+
+         // 1-30 are done....
+// adding more rows..
+
+];
+
+
+let currentSortColumn = -1; // Initially, no column is sorted
+let isAscending = true; // Initially, sorting is in ascending order
+
+document.addEventListener('DOMContentLoaded', function () {
+    renderTable(data);
+    
+    const searchInput = document.getElementById('search');
+    searchInput.addEventListener('input', function () {
+        const searchTerm = this.value.toLowerCase();
+        const filteredData = data.filter(row => row.some(cell => cell.toString().toLowerCase().includes(searchTerm)));
+        renderTable(filteredData);
+    });
+});
+
+function renderTable(data) {
+    const tableBody = document.getElementById('tableBody');
+    tableBody.innerHTML = '';
+
+    data.forEach(row => {
+        const tr = document.createElement('tr');
+
+        row.forEach((cell, index) => {
+            const td = document.createElement('td');
+
+            if (index >= 3 && index <= 7) {
+                const img = document.createElement('img');
+                img.src = cell;
+                img.alt = `Finger ${index - 1}`;
+                img.style.width = '100px'; // Set width to auto for actual size
+                img.style.height = '100px'; // Set height to auto for actual size
+                img.style.transform = 'scaleX(-1)'; // Mirror horizontally
+                img.style.filter = 'invert(100%)'; // Color invert
+                td.appendChild(img);
+            } else {
+                td.textContent = cell;
+            }
+
+            tr.appendChild(td);
+        });
+
+        tableBody.appendChild(tr);
+    });
+
+    updateTotalCount(data.length);
+}
+
+function updateTotalCount(count) {
+    const totalCountElement = document.getElementById('totalCount');
+    totalCountElement.textContent = `Total Count: ${count}`;
+}
+
+function sortTable(columnIndex) {
+    if (currentSortColumn === columnIndex) {
+        // If the same column is clicked again, reverse the sorting order
+        isAscending = !isAscending;
+    } else {
+        // If a new column is clicked, set the sorting order to ascending
+        isAscending = true;
+        currentSortColumn = columnIndex;
+    }
+
+    data.sort((a, b) => {
+        const valueA = a[currentSortColumn];
+        const valueB = b[currentSortColumn];
+
+        // Customize the comparison logic based on the data type (string, number, etc.)
+        return isAscending ? valueA.localeCompare(valueB) : valueB.localeCompare(valueA);
+    });
+
+    renderTable(data);
+}
