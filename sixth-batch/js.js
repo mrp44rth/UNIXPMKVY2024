e13b48478b47e4fe424e1b126242fe7abc54508b
mrp44rth <113108394+mrp44rth@users.noreply.github.com>
2024-03-22 17:38:37 +0530

    Add files via upload

---
+++ sixth-batch/js.js
@@ -0,0 +1,112 @@
+// Sample data (you can replace this with your actual data)
+let i = 1;
+const data = [
+ // add data here
+[i++,"Abhishek Dubey","18944043","./sixth batch/Abhishek Dubey_18944043/1.bmp","./sixth batch/Abhishek Dubey_18944043/2.bmp","./sixth batch/Abhishek Dubey_18944043/3.bmp","./sixth batch/Abhishek Dubey_18944043/4.bmp","./sixth batch/Abhishek Dubey_18944043/5.bmp"],
+[i++,"Toyaj Yadav","1767773","./sixth batch/Toyaj Yadav_1767773/1.bmp","./sixth batch/Toyaj Yadav_1767773/2.bmp","./sixth batch/Toyaj Yadav_1767773/3.bmp","./sixth batch/Toyaj Yadav_1767773/4.bmp","./sixth batch/Toyaj Yadav_1767773/5.bmp"],
+[i++,"Abhishek Mishra","12897974","./sixth batch/Abhishek Mishra_12897974/1.bmp","./sixth batch/Abhishek Mishra_12897974/2.bmp","./sixth batch/Abhishek Mishra_12897974/3.bmp","./sixth batch/Abhishek Mishra_12897974/4.bmp","./sixth batch/Abhishek Mishra_12897974/5.bmp"],
+[i++,"Akash Kumar","75037251","./sixth batch/Akash Kumar_75037251/1.bmp","./sixth batch/Akash Kumar_75037251/2.bmp","./sixth batch/Akash Kumar_75037251/3.bmp","./sixth batch/Akash Kumar_75037251/4.bmp","./sixth batch/Akash Kumar_75037251/5.bmp"],
+[i++,"Udayraj Gautam","6510926","./sixth batch/Udayraj Gautam_6510926/1.bmp","./sixth batch/Udayraj Gautam_6510926/2.bmp","./sixth batch/Udayraj Gautam_6510926/3.bmp","./sixth batch/Udayraj Gautam_6510926/4.bmp","./sixth batch/Udayraj Gautam_6510926/5.bmp"],
+[i++,"Vimla","28808211","./sixth batch/Vimla_28808211/1.bmp","./sixth batch/Vimla_28808211/2.bmp","./sixth batch/Vimla_28808211/3.bmp","./sixth batch/Vimla_28808211/4.bmp","./sixth batch/Vimla_28808211/5.bmp"],
+[i++,"Anjali Prajapati","52844106","./sixth batch/Anjali Prajapati_52844106/1.bmp","./sixth batch/Anjali Prajapati_52844106/2.bmp","./sixth batch/Anjali Prajapati_52844106/3.bmp","./sixth batch/Anjali Prajapati_52844106/4.bmp","./sixth batch/Anjali Prajapati_52844106/5.bmp"],
+[i++,"Yash Chaurasiya","68349581","./sixth batch/Yash Chaurasiya_68349581/1.bmp","./sixth batch/Yash Chaurasiya_68349581/2.bmp","./sixth batch/Yash Chaurasiya_68349581/3.bmp","./sixth batch/Yash Chaurasiya_68349581/4.bmp","./sixth batch/Yash Chaurasiya_68349581/5.bmp"],
+[i++,"Bajarangi Maurya","5145122","./sixth batch/Bajarangi Maurya_5145122/1.bmp","./sixth batch/Bajarangi Maurya_5145122/2.bmp","./sixth batch/Bajarangi Maurya_5145122/3.bmp","./sixth batch/Bajarangi Maurya_5145122/4.bmp","./sixth batch/Bajarangi Maurya_5145122/5.bmp"],
+[i++,"Durga Devi","11253193","./sixth batch/Durga Devi_11253193/1.bmp","./sixth batch/Durga Devi_11253193/2.bmp","./sixth batch/Durga Devi_11253193/3.bmp","./sixth batch/Durga Devi_11253193/4.bmp","./sixth batch/Durga Devi_11253193/5.bmp"],
+[i++,"Faheem Ahmad","95666395","./sixth batch/Faheem Ahmad_95666395/1.bmp","./sixth batch/Faheem Ahmad_95666395/2.bmp","./sixth batch/Faheem Ahmad_95666395/3.bmp","./sixth batch/Faheem Ahmad_95666395/4.bmp","./sixth batch/Faheem Ahmad_95666395/5.bmp"],
+[i++,"Fauziya Khatoon","61961959","./sixth batch/Fauziya Khatoon_61961959/1.bmp","./sixth batch/Fauziya Khatoon_61961959/2.bmp","./sixth batch/Fauziya Khatoon_61961959/3.bmp","./sixth batch/Fauziya Khatoon_61961959/4.bmp","./sixth batch/Fauziya Khatoon_61961959/5.bmp"],
+[i++,"Hooma","29746690","./sixth batch/Hooma_29746690/1.bmp","./sixth batch/Hooma_29746690/2.bmp","./sixth batch/Hooma_29746690/3.bmp","./sixth batch/Hooma_29746690/4.bmp","./sixth batch/Hooma_29746690/5.bmp"],
+[i++,"Kavita Bharati","18168153","./sixth batch/Kavita Bharati_18168153/1.bmp","./sixth batch/Kavita Bharati_18168153/2.bmp","./sixth batch/Kavita Bharati_18168153/3.bmp","./sixth batch/Kavita Bharati_18168153/4.bmp","./sixth batch/Kavita Bharati_18168153/5.bmp"],
+[i++,"Km Priti Yadav","48490324","./sixth batch/Km Priti Yadav_48490324/1.bmp","./sixth batch/Km Priti Yadav_48490324/2.bmp","./sixth batch/Km Priti Yadav_48490324/3.bmp","./sixth batch/Km Priti Yadav_48490324/4.bmp","./sixth batch/Km Priti Yadav_48490324/5.bmp"],
+[i++,"Komal Prajapati","81874148","./sixth batch/Komal Prajapati_81874148/1.bmp","./sixth batch/Komal Prajapati_81874148/2.bmp","./sixth batch/Komal Prajapati_81874148/3.bmp","./sixth batch/Komal Prajapati_81874148/4.bmp","./sixth batch/Komal Prajapati_81874148/5.bmp"],
+[i++,"Lav Anand Shreevastav","19381913","./sixth batch/Lav Anand Shreevastav_19381913/1.bmp","./sixth batch/Lav Anand Shreevastav_19381913/2.bmp","./sixth batch/Lav Anand Shreevastav_19381913/3.bmp","./sixth batch/Lav Anand Shreevastav_19381913/4.bmp","./sixth batch/Lav Anand Shreevastav_19381913/5.bmp"],
+[i++,"Manish Kumar","3358046","./sixth batch/Manish Kumar_3358046/1.bmp","./sixth batch/Manish Kumar_3358046/2.bmp","./sixth batch/Manish Kumar_3358046/3.bmp","./sixth batch/Manish Kumar_3358046/4.bmp","./sixth batch/Manish Kumar_3358046/5.bmp"],
+[i++,"Meenu Pal","73314160","./sixth batch/Meenu Pal_73314160/1.bmp","./sixth batch/Meenu Pal_73314160/2.bmp","./sixth batch/Meenu Pal_73314160/3.bmp","./sixth batch/Meenu Pal_73314160/4.bmp","./sixth batch/Meenu Pal_73314160/5.bmp"],
+[i++,"Mohd Gufran Rahmani","53344768","./sixth batch/Mohd Gufran Rahmani_53344768/1.bmp","./sixth batch/Mohd Gufran Rahmani_53344768/2.bmp","./sixth batch/Mohd Gufran Rahmani_53344768/3.bmp","./sixth batch/Mohd Gufran Rahmani_53344768/4.bmp","./sixth batch/Mohd Gufran Rahmani_53344768/5.bmp"],
+[i++,"Muskan Bano","13315991","./sixth batch/Muskan Bano_13315991/1.bmp","./sixth batch/Muskan Bano_13315991/2.bmp","./sixth batch/Muskan Bano_13315991/3.bmp","./sixth batch/Muskan Bano_13315991/4.bmp","./sixth batch/Muskan Bano_13315991/5.bmp"],
+[i++,"Navneet Sahu","75413623","./sixth batch/Navneet Sahu_75413623/1.bmp","./sixth batch/Navneet Sahu_75413623/2.bmp","./sixth batch/Navneet Sahu_75413623/3.bmp","./sixth batch/Navneet Sahu_75413623/4.bmp","./sixth batch/Navneet Sahu_75413623/5.bmp"],
+[i++,"Priyanka Bind","72748916","./sixth batch/Priyanka Bind_72748916/1.bmp","./sixth batch/Priyanka Bind_72748916/2.bmp","./sixth batch/Priyanka Bind_72748916/3.bmp","./sixth batch/Priyanka Bind_72748916/4.bmp","./sixth batch/Priyanka Bind_72748916/5.bmp"],
+[i++,"Raman Kumar","78712287","./sixth batch/Raman Kumar_78712287/1.bmp","./sixth batch/Raman Kumar_78712287/2.bmp","./sixth batch/Raman Kumar_78712287/3.bmp","./sixth batch/Raman Kumar_78712287/4.bmp","./sixth batch/Raman Kumar_78712287/5.bmp"],
+[i++,"Sanju Yadav","99890119","./sixth batch/Sanju Yadav_99890119/1.bmp","./sixth batch/Sanju Yadav_99890119/2.bmp","./sixth batch/Sanju Yadav_99890119/3.bmp","./sixth batch/Sanju Yadav_99890119/4.bmp","./sixth batch/Sanju Yadav_99890119/5.bmp"],
+[i++,"Sayma Parveen","99497002","./sixth batch/Sayma Parveen_99497002/1.bmp","./sixth batch/Sayma Parveen_99497002/2.bmp","./sixth batch/Sayma Parveen_99497002/3.bmp","./sixth batch/Sayma Parveen_99497002/4.bmp","./sixth batch/Sayma Parveen_99497002/5.bmp"],
+[i++,"Shamsher","75390165","./sixth batch/Shamsher_75390165/1.bmp","./sixth batch/Shamsher_75390165/2.bmp","./sixth batch/Shamsher_75390165/3.bmp","./sixth batch/Shamsher_75390165/4.bmp","./sixth batch/Shamsher_75390165/5.bmp"],
+[i++,"Shiv shankar Yadav","63713342","./sixth batch/Shiv shankar Yadav_63713342/1.bmp","./sixth batch/Shiv shankar Yadav_63713342/2.bmp","./sixth batch/Shiv shankar Yadav_63713342/3.bmp","./sixth batch/Shiv shankar Yadav_63713342/4.bmp","./sixth batch/Shiv shankar Yadav_63713342/5.bmp"],
+[i++,"SHIVANGEE","63943766","./sixth batch/SHIVANGEE_63943766/1.bmp","./sixth batch/SHIVANGEE_63943766/2.bmp","./sixth batch/SHIVANGEE_63943766/3.bmp","./sixth batch/SHIVANGEE_63943766/4.bmp","./sixth batch/SHIVANGEE_63943766/5.bmp"],
+
+
+         // 1-30 are done....
+// adding more rows..
+
+];
+
+
+let currentSortColumn = -1; // Initially, no column is sorted
+let isAscending = true; // Initially, sorting is in ascending order
+
+document.addEventListener('DOMContentLoaded', function () {
+    renderTable(data);
+    
+    const searchInput = document.getElementById('search');
+    searchInput.addEventListener('input', function () {
+        const searchTerm = this.value.toLowerCase();
+        const filteredData = data.filter(row => row.some(cell => cell.toString().toLowerCase().includes(searchTerm)));
+        renderTable(filteredData);
+    });
+});
+
+function renderTable(data) {
+    const tableBody = document.getElementById('tableBody');
+    tableBody.innerHTML = '';
+
+    data.forEach(row => {
+        const tr = document.createElement('tr');
+
+        row.forEach((cell, index) => {
+            const td = document.createElement('td');
+
+            if (index >= 3 && index <= 7) {
+                const img = document.createElement('img');
+                img.src = cell;
+                img.alt = `Finger ${index - 1}`;
+                img.style.width = '100px'; // Set width to auto for actual size
+                img.style.height = '100px'; // Set height to auto for actual size
+                img.style.transform = 'scaleX(-1)'; // Mirror horizontally
+                img.style.filter = 'invert(100%)'; // Color invert
+                td.appendChild(img);
+            } else {
+                td.textContent = cell;
+            }
+
+            tr.appendChild(td);
+        });
+
+        tableBody.appendChild(tr);
+    });
+
+    updateTotalCount(data.length);
+}
+
+function updateTotalCount(count) {
+    const totalCountElement = document.getElementById('totalCount');
+    totalCountElement.textContent = `Total Count: ${count}`;
+}
+
+function sortTable(columnIndex) {
+    if (currentSortColumn === columnIndex) {
+        // If the same column is clicked again, reverse the sorting order
+        isAscending = !isAscending;
+    } else {
+        // If a new column is clicked, set the sorting order to ascending
+        isAscending = true;
+        currentSortColumn = columnIndex;
+    }
+
+    data.sort((a, b) => {
+        const valueA = a[currentSortColumn];
+        const valueB = b[currentSortColumn];
+
+        // Customize the comparison logic based on the data type (string, number, etc.)
+        return isAscending ? valueA.localeCompare(valueB) : valueB.localeCompare(valueA);
+    });
+
+    renderTable(data);
+}
